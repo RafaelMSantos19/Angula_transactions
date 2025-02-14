4913ea1a464cfeff173fec91150b6bab643c5680
Rafael Santos <rafarag40@gmail.com>
2025-02-14 17:09:12 -0300

    feat: config redirect pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,18 @@ import { FilterPainelComponent } from './filter-painel/filter-painel.component';
 import { TransactionsPainelComponent } from './transactions-painel/transactions-painel.component';
 import { ListPageComponent } from './list-page/list-page.component';
 import { NewTransactionPageComponent } from './new-transaction-page/new-transaction-page.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { TitleOfPageComponent } from './title-of-page/title-of-page.component';
+
+const routes: Routes =[
+
+  { path: '', component: HomePageComponent},
+  { path: 'list', component: ListPageComponent},
+  { path: 'newTransaction', component: NewTransactionPageComponent},
+  { path: '**', redirectTo: ''}
+
+]
 
 @NgModule({
   declarations: [
@@ -16,15 +28,21 @@ import { NewTransactionPageComponent } from './new-transaction-page/new-transact
     FilterPainelComponent,
     TransactionsPainelComponent,
     ListPageComponent,
-    NewTransactionPageComponent
+    NewTransactionPageComponent,
+    HomePageComponent,
+    TitleOfPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [
     provideClientHydration(withEventReplay())
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports:[
+    RouterModule
+  ]
 })
 export class AppModule { }
