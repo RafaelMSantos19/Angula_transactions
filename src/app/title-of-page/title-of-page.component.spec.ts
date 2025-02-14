8d3deabf865ca376289322e216e30e39e8545012
Rafael Santos <rafarag40@gmail.com>
2025-02-14 17:10:23 -0300

    feat: create tittle-of-page component

---
+++ src/app/title-of-page/title-of-page.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TitleOfPageComponent } from './title-of-page.component';
+
+describe('TitleOfPageComponent', () => {
+  let component: TitleOfPageComponent;
+  let fixture: ComponentFixture<TitleOfPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [TitleOfPageComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(TitleOfPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/title-of-page/title-of-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-title-of-page',
+  standalone: false,
+  templateUrl: './title-of-page.component.html',
+  styleUrl: './title-of-page.component.css'
+})
+export class TitleOfPageComponent {
+
+}
