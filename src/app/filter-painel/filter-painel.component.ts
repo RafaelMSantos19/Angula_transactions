a50856bb349fa88f4c522f5dab7a85764f53a44d
Rafael Santos <rafarag40@gmail.com>
2025-02-17 18:11:44 -0300

    feat: implement request with filters

--- src/app/filter-painel/filter-painel.component.ts
+++ src/app/filter-painel/filter-painel.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-filter-painel',
@@ -7,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrl: './filter-painel.component.css'
 })
 export class FilterPainelComponent {
+
+  @Output() filterChange = new EventEmitter<any>();
+
+  transationFiltersForm: FormGroup;
+
+  constructor(private fb: FormBuilder){
+    this.transationFiltersForm = this.fb.group({
+      id: [''],
+      dataInicial: [''],
+      dataFinal: [''],
+      valor: [''],
+      status: [''],
+      metodo_pagamento: ['']
+    })
+  }
+
+  onSubmit(){
+    if( this.transationFiltersForm.valid){
+      const formData= this.transationFiltersForm.value
+      this.filterChange.emit(formData);
+    }
+  }
 
 }
--- src/app/list-page/list-page.component.ts
+++ src/app/list-page/list-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-list-page',
@@ -6,12 +7,74 @@ import { Component } from '@angular/core';
   templateUrl: './list-page.component.html',
   styleUrl: './list-page.component.css'
 })
+
 export class ListPageComponent {
 
   titulo?: string = 'List Page';
+
+  apiUrl = 'http://localhost:3090/gettransactions';
 
   transacoesExemplo = [
     { id: 1, data: '2025-02-04T03:27:27.499Z', valor: "R$ "+25, status: "confirmada", mp: "PIX" }
   ];
 
+  constructor(private http: HttpClient) {}
+
+  formatData(data: any, secondsWithMiliseconds: any): string {
+    const date = new Date(data);
+    
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+
+    const formattedData = `${year}-${month}-${day} ${hours}:${minutes}`;
+    
+    return formattedData + secondsWithMiliseconds;
+  }
+  
+  onFilterChange(filters: any): void {
+    // debug
+    console.log('Filtros recebidos: ', filters)
+
+    const requestId = filters.id
+    const requestDataInicial = this.formatData(filters.dataInicial,':00.000')
+    const requestDataFinal = this.formatData(filters.dataFinal,':59.999')
+    const requestValor = filters.valor
+    const requestStatus = filters.status
+    const requestMetodoPagamento = filters.metodo_pagamento
+
+    // debug
+    console.log({
+      requestId,
+      requestDataInicial,
+      requestDataFinal,
+      requestValor,
+      requestStatus,
+      requestMetodoPagamento
+    });
+
+    const headers = new HttpHeaders({
+      'id': requestId?.toString() || '',
+      'data_inicial': requestDataInicial,
+      'data_final': requestDataFinal,
+      'valor': requestValor?.toString() || '',
+      'status': requestStatus || '',
+      'metodo_pagamento': requestMetodoPagamento || ''
+    })
+
+    this.http.get(this.apiUrl, { headers }).subscribe(
+      response => {
+        console.log('Resposta da Api: ', response)
+      },
+      error =>{
+        console.log('Erro na requisição: ', error)
+      }
+    )
+
+  }
+
 }
+
+
