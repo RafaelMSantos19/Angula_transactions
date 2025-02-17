3b5c07dcc09c6caa274a135081826af425e9fda3
Rafael Santos <rafarag40@gmail.com>
2025-02-17 16:48:07 -0300

    feat: create transaction table view

--- src/app/list-page/list-page.component.ts
+++ src/app/list-page/list-page.component.ts
@@ -10,4 +10,8 @@ export class ListPageComponent {
 
   titulo?: string = 'List Page';
 
+  transacoesExemplo = [
+    { id: 1, data: '2025-02-04T03:27:27.499Z', valor: "R$ "+25, status: "confirmada", mp: "PIX" }
+  ];
+
 }
